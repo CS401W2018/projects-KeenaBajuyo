e8869136c408c1db2f6dd1c5d76c148d9bb9d80c
Keena Bajuyo <bajuyokeena@gmail.com>
2025-04-09 15:45:59 -1000

    wow syncs

--- final/Project/form.js
+++ final/Project/form.js
@@ -36,7 +36,7 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
 
     console.log(formData);  
     const xhr = new XMLHttpRequest();
-    xhr.open('POST', 'submit.json', true); 
+    xhr.open('GET', 'submit.json', true); 
     xhr.setRequestHeader('Content-Type', 'application/json;charset=UTF-8');
 
     xhr.onreadystatechange = function() {
